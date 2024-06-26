41deb18f7417a618393b921f4df887253b9c3ef2
Chaeyun-Lee <145410502+Chaeyun-Lee@users.noreply.github.com>
2024-06-26 18:09:52 +0900

    Add files via upload

--- js/thaiHeader.js
+++ js/thaiHeader.js
@@ -1,11 +1,5 @@
-window.onload = function () {
-    //페이지 로드 시 잠깐 보이는 트랜지션 없애는 용도
-    document.querySelector("body").classList.remove("preload");
-  
-//스크롤 시 헤더 fixed, topBtn 생성, mainPage 풀페이지 바뀌기
 window.addEventListener("scroll",function(){
     const header = document.querySelector("header");
-    const imgBox = document.querySelector(".imgBox");
     const topBtn =document.querySelector(".topBtn");
     console.log(scrollY);
     if(this.window.scrollY > 0) {
@@ -17,13 +11,9 @@ window.addEventListener("scroll",function(){
         header.style.border = "1px solid #eee";
         header.style.backgroundColor = "#ffffff";
         if(this.window.scrollY > 400){
-            imgBox.style.width = '100%';
-            imgBox.style.borderRadius = "0";
             topBtn.style.visibility = "visible";
         } else if (this.window.scrollY < 400){
             topBtn.style.visibility = "hidden";
-            imgBox.style.width = '80vw';
-            imgBox.style.borderRadius = "100px";
         }
     } else if (this.window.scrollY == 0){
         header.style.border = "none";
@@ -31,8 +21,7 @@ window.addEventListener("scroll",function(){
         header.style.left = "50%";
         header.style.transform = 'translateX(-50%)';
     }
-})
-};
+});
 
 //드롭다운 메뉴
 $(function (){
